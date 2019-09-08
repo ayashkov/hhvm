ab51290e96f182d41c2797d698e1120febe149e1
Alexei Yashkov <alexei.yashkov@yashkov.org>
2019-09-08 12:44:51 -0400

    Organize imports

--- src/test/java/org/yashkov/hhvm/VmTest.java
+++ src/test/java/org/yashkov/hhvm/VmTest.java
@@ -1,6 +1,6 @@
 package org.yashkov.hhvm;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
